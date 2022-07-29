64c73601ec9d5fa4d889dde2460a0a62b9daf81e
nhab <n.habibollahi@yahoo.com>
2022-07-29 23:18:00 +0430

    DBContext and student model added

---
+++ RestfullAPI/DB/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentsRestfullAPI.DB
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        { 
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);   
+        }
+    }
+}
---
+++ RestfullAPI/DB/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentsRestfullAPI.DB
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public int Age { get; set; }
+    }
+}
