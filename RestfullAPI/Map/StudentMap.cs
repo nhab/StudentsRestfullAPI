02e9da835cfa7bcfa80e38c36c163ad2aef38680
nhab <n.habibollahi@yahoo.com>
2022-07-29 23:24:50 +0430

    Maps Added

---
+++ RestfullAPI/Map/StudentMap.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudentsRestfullAPI.DB;
+
+namespace StudentsRestfullAPI.Map
+{
+    public class StudentMap : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.ToTable(nameof(Student));   
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.FName)
+                .IsRequired()
+                .HasMaxLength(100);
+            builder.Property(x => x.FName)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
